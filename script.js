5707db4d671f4bcfd464993caff5bf3dfcdca2bc
TAKURO-NISHIO <78525369+TAKURO-NISHIO@users.noreply.github.com>
2021-02-04 15:36:07 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+'use strict'
+
+// function draw() {
+//   // ランダムな整数を作成（1-6の例）
+//   let randomNumber = Math.floor(Math.random() * 6 + 1);
+
+//   // くじの結果画像を表示させる
+//   document.getElementsByClassName("manbo)[0].setAttribute('src', `images/manbo${randomNumber}.png`);
+
+//   // ボタンの文字を変更する
+//   document.querySelectorAll("button")[0].innerHTML = "もう一度引く";
+// }
